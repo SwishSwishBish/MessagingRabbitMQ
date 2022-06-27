066c11313714d2b1ea7153fadc97fcb1601d4c07
Sena <darkphoenixq@gmail.com>
2022-06-27 16:18:02 +0300

    connection created

---
+++ src/test/java/com/sena/messagingrabbitmq/MessagingRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.messagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
