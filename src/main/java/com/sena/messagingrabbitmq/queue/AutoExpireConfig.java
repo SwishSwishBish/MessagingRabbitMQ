b629224751fdb02467a4643bda5b11675c582925
Sena <darkphoenixq@gmail.com>
2022-06-30 08:38:50 +0300

    auto expire

---
+++ src/main/java/com/sena/messagingrabbitmq/queue/AutoExpireConfig.java
@@ -0,0 +1,47 @@
+package com.sena.messagingrabbitmq.queue;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class AutoExpireConfig {
+    @Value("${rabbitmq.expire.queue}")
+    private String QUEUE_NAME;
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    @PostConstruct
+    public void init()
+    {
+        amqpAdmin.declareQueue(createExpireQueue());
+    }
+
+    Queue createExpireQueue()
+    {
+        return QueueBuilder.durable(QUEUE_NAME)
+                .expires(10000)
+                .build();
+    }
+
+    @Bean
+    public AmqpTemplate defaultExpireExchange(ConnectionFactory connectionFactory, MessageConverter messageConverter)
+    {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        rabbitTemplate.setRoutingKey(QUEUE_NAME);
+
+        return rabbitTemplate;
+    }
+}
